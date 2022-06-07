80a8aa6b6636ddabb48b6fb6e662dc98f196610f
MaksimDvoretskii <mdmmdams@mail.ru>
2022-06-08 00:59:59 +0400

    add testNG

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -3,10 +3,10 @@ package ru.netology.service;
 public class CashbackHackServiceTest {
 
     @org.testng.annotations.Test
-    public void testRemain() {
+    public void shouldCalculateReminder() {
         CashbackHackService hack = new CashbackHackService();
 
-        int actual = hack.remain(900)
+        int actual = hack.remain(900);
         int expected = 100;
 
         org.testng.Assert.assertEquals(actual, expected);
